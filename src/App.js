51108abb4ed565bd4108fa4bc9864f0136401585
Amer Kulovic <amer.kulovic@yahoo.com>
2023-06-21 12:44:03 -0500

    outputed the search results

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import Nav from "./components/Nav";
 import { useState } from "react";
 
 function App() {
-  let [movie, setMovie] = useState(null);
+  let [movies, setMovies] = useState(null);
   let [name, setName] = useState("");
 
   const valueHandler = (event) => {
     setName(event.target.value);
-    console.log(name);
   };
 
   const searchHandler = async () => {
-    await fetch(`http://www.omdbapi.com/?t=${name}&apikey=f14ca85d
-  `)
+    await fetch(`https://www.omdbapi.com/?apikey=f14ca85d&s=${name}`)
       .then((response) => response.json())
       .then((data) => {
-        setMovie(data);
-        console.log(movie);
+        setMovies(data);
+        console.log(movies.Search);
       });
   };
 
   return (
     <>
       <Nav search={searchHandler} value={valueHandler} />
-      <h1>{movie.Title}</h1>
+      <container className="flex flex-wrap justify-around">
+        {movies &&
+          movies.Search.map((movie) => (
+            <div className="text-center mx-2">
+              <h1>{movie.Title}</h1>
+              <img src={movie.Poster} />
+            </div>
+          ))}
+      </container>
     </>
   );
 }
