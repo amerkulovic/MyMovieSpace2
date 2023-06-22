6243e60bf59c385d286e9fc313acb1095894ddf5
Amer Kulovic <amer.kulovic@yahoo.com>
2023-06-22 13:30:24 -0500

    imported css styling for the movie cards

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
+import search from "./../svgs/search.svg";
+
 const Nav = (props) => {
   return (
     <div className="bg-gradient-to-r from-black via-red-700 to-black flex items-center justify-between py-4">
-      <div className="mx-2">
-        <input placeholder="Find a Movie!" onChange={props.value} />
-        <button onClick={props.search}>Search</button>
+      <div className="ml-4 flex items-center">
+        <input className="h-14 px-4 rounded-tl-xl rounded-bl-xl" placeholder="Find a Movie!" onChange={props.value} />
+        <button className="bg-white p-4 h-14 rounded-tr-xl rounded-br-xl" onClick={props.search}>
+          <img className="h-5 w-5" src={search} />
+        </button>
       </div>
       <h1 className="header-font text-white text-6xl">MyMovieSpace</h1>
       <section>
