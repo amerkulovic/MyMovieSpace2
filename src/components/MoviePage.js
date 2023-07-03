b72e9d7f6548c84e2922d728484454c8398058eb
Amer Kulovic <amer.kulovic@yahoo.com>
2023-07-03 10:58:33 -0500

    improved the movie page

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -3,16 +3,25 @@ import notFoundImg from "./../images/notfoundimg.png";
 const MoviePage = () => {
   return (
     <div className="flex flex-row justify-center">
-      <div className="mr-5 mt-10">
-        <img src={notFoundImg} className="h-4/5 w-96" />
+      <div className="mr-5 my-10">
+        <img src={notFoundImg} className="h-[500px] w-96" />
       </div>
-      <section className="text-white mt-10">
-        <div>
+      <div className="text-white mt-10">
+        <section className="border-b-[0.5px] border-white mb-5">
           <h1 className="movie-header text-4xl">Indiana Jones and the Dial of Destiny</h1>
           <p>2023 directed by James Mangold</p>
-        </div>
-        <p className="w-96 mt-4">Finding himself in a new era, approaching retirement, Indy wrestles with fitting into a world that seems to have outgrown him. But as the tentacles of an all-too-familiar evil return in the form of an old rival, Indy must don his hat and pick up his whip once more to make sure an ancient and powerful artifact doesn’t fall into the wrong hands.</p>
-      </section>
+        </section>
+        <p className="w-[480px] mt-4">Finding himself in a new era, approaching retirement, Indy wrestles with fitting into a world that seems to have outgrown him. But as the tentacles of an all-too-familiar evil return in the form of an old rival, Indy must don his hat and pick up his whip once more to make sure an ancient and powerful artifact doesn’t fall into the wrong hands.</p>
+        <section>
+          <h2 className="movie-header text-xl mt-5 mb-5 border-b-[0.5px] border-white">Notable Actors</h2>
+          <p>Harrison Ford, Mads Mikkelson</p>
+        </section>
+        <section>
+          <h2 className="movie-header text-xl mt-5 mb-5 border-b-[0.5px] border-white">Reviews</h2>
+          <p>Rotten Tomatoes 93%</p>
+          <p>iMDB 8/10</p>
+        </section>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "h-100": "32rem",
+      },
+    },
   },
   plugins: [],
 };
