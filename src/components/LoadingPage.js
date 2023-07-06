e052e553b440c1a5f8285b19c21ec31dc9f6885a
Amer Kulovic <amer.kulovic@yahoo.com>
2023-07-05 19:12:25 -0500

    created the loading page

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,8 @@
+const LoadingPage = () => {
+  return (
+    <div className="h-[600px] flex justify-center items-center">
+      <h1 className="movie-header text-7xl">Loading...</h1>
+    </div>
+  );
+};
+export default LoadingPage;
--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import notFoundImg from "./../images/notfoundimg.png";
+import LoadingPage from "./LoadingPage";
 import ReviewCard from "./ReviewCard";
 
 const MoviePage = () => {
@@ -23,42 +24,45 @@ const MoviePage = () => {
   }, [movie]);
 
   return (
-    movie && (
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-center">
-          <div className="mr-5 my-10">
-            <img src={movie.Poster} className="h-[500px] w-96" />
-          </div>
-          <div className="text-white mt-10">
-            <section className="border-b-[0.5px] border-white mb-5">
-              <h1 className="movie-header text-5xl w-[800px]">{movie.Title}</h1>
-              <p>
-                {movie.Year} directed by {movie.Director}
-              </p>
-            </section>
-            <p className="w-[480px] mt-4">{movie.Plot}</p>
-            <section>
-              <h2 className="movie-header text-xl mt-5 mb-5 border-b-[0.5px] border-white">Notable Actors</h2>
-              <p>{movie.Actors}</p>
-            </section>
-            <section>
-              <h2 className="movie-header text-xl mt-5 mb-5 border-b-[0.5px] border-white">Reviews</h2>
-              {movie.Ratings.map((rating) => (
+    <>
+      {!movie && <LoadingPage />}
+      {movie && (
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-center">
+            <div className="mr-5 my-10">
+              <img src={movie.Poster} className="h-[500px] w-96" />
+            </div>
+            <div className="text-white mt-10">
+              <section className="border-b-[0.5px] border-white mb-5">
+                <h1 className="movie-header text-5xl w-[800px]">{movie.Title}</h1>
                 <p>
-                  {rating.Source} {rating.Value}
+                  {movie.Year} directed by {movie.Director}
                 </p>
-              ))}
-            </section>
+              </section>
+              <p className="w-[480px] mt-4">{movie.Plot}</p>
+              <section>
+                <h2 className="movie-header text-xl mt-5 mb-5 border-b-[0.5px] border-white">Notable Actors</h2>
+                <p>{movie.Actors}</p>
+              </section>
+              <section>
+                <h2 className="movie-header text-xl mt-5 mb-5 border-b-[0.5px] border-white">Reviews</h2>
+                {movie.Ratings.map((rating) => (
+                  <p>
+                    {rating.Source} {rating.Value}
+                  </p>
+                ))}
+              </section>
+            </div>
           </div>
+          <section className="mb-10 flex flex-col items-center">
+            <h1 className="flex justify-center movie-header text-4xl border-b-[0.5px] border-white w-[900px] mb-4">User Reviews</h1>
+            <ReviewCard title="Amazing Movie" date="7/3/23" text="It was great to see Harrison Ford play the role of Indiana Jones one last time!It was great to see Harrison Ford play the role of Indiana Jones one last time!It was great to see Harrison Ford play the role of Indiana Jones one last time!It was great to see Harrison Ford play the role of Indiana Jones one last time!" username="Akulovic" />
+            <ReviewCard title="Amazing Movie" date="7/3/23" text="It was great to see Harrison Ford play the role of Indiana Jones one last time!" username="Akulovic" />
+            <ReviewCard title="Amazing Movie" date="7/3/23" text="It was great to see Harrison Ford play the role of Indiana Jones one last time!" username="Akulovic" />
+          </section>
         </div>
-        <section className="mb-10 flex flex-col items-center">
-          <h1 className="flex justify-center movie-header text-4xl border-b-[0.5px] border-white w-[900px] mb-4">User Reviews</h1>
-          <ReviewCard title="Amazing Movie" date="7/3/23" text="It was great to see Harrison Ford play the role of Indiana Jones one last time!It was great to see Harrison Ford play the role of Indiana Jones one last time!It was great to see Harrison Ford play the role of Indiana Jones one last time!It was great to see Harrison Ford play the role of Indiana Jones one last time!" username="Akulovic" />
-          <ReviewCard title="Amazing Movie" date="7/3/23" text="It was great to see Harrison Ford play the role of Indiana Jones one last time!" username="Akulovic" />
-          <ReviewCard title="Amazing Movie" date="7/3/23" text="It was great to see Harrison Ford play the role of Indiana Jones one last time!" username="Akulovic" />
-        </section>
-      </div>
-    )
+      )}
+    </>
   );
 };
 
