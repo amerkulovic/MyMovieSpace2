af97c6f17d9f13f4424156176d3996767570411d
Amer Kulovic <amer.kulovic@yahoo.com>
2023-09-05 11:08:19 -0500

    made progress on the mobile nav

--- client/src/components/HamburgerMenu.js
+++ client/src/components/HamburgerMenu.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import NavLinks from "./NavLinks";
+import search from "./../svgs/search.svg";
+import { Link } from "react-router-dom";
 
-const HamburgerMenu = ({ isOpen, toggleMenu }) => {
+const HamburgerMenu = (props) => {
   return (
     <div>
-      <div className={`fixed z-10 left-0 top-0 h-full w-80 max-sm:w-60 pt-20 bg-red-800 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out`}>
-        <NavLinks buttonClick={toggleMenu} />
+      <div className={`fixed z-10 left-0 top-0 h-full w-80 max-sm:w-60 pt-20 bg-red-800 transform ${props.isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out`}>
+        <NavLinks buttonClick={props.toggleMenu} />
+        <div className="opacity-70 hover:opacity-100 flex items-center max-xl:pl-2 max-xl:mt-10">
+          <input className="h-14 max-sm:w-[165px] px-4 rounded-tl-xl rounded-bl-xl focus:outline-none" placeholder="Find a Movie!" onChange={props.value} />
+          <Link to="/search">
+            <button className="bg-white p-4 h-14 rounded-tr-xl rounded-br-xl " onClick={props.search}>
+              <img className="h-5 w-5" src={search} />
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -14,13 +14,21 @@ const Nav = (props) => {
   };
 
   return (
-    <div className="bg-gradient-to-r from-black via-red-700 to-black flex items-center justify-center py-4">
+    <div className="bg-gradient-to-r from-black via-red-700 to-black flex items-center max-xl:justify-between justify-center py-4">
+      <div className="hidden max-xl:flex">
+        <button onClick={toggleMenu} className="z-20 ml-5 px-3 py-2 bg-white bg-opacity-80 hover:bg-opacity-100 rounded-lg text-red-800 font-bold text-2xl ">
+          X
+        </button>
+        <HamburgerMenu isOpen={isOpen} toggleMenu={toggleMenu} search={props.search} value={props.value} />
+      </div>
       <Link to="/">
         <h1 className="header-font text-white text-6xl mr-10 max-xl:m-0">MyMovieSpace</h1>
       </Link>
-      <div className="ml-4 flex items-center max-xl:hidden">
-        <NavLinks />
-        <div className="opacity-70 hover:opacity-100 flex items-center">
+      <div className="ml-4 flex items-center">
+        <div className="max-xl:hidden">
+          <NavLinks />
+        </div>
+        <div className="opacity-70 hover:opacity-100 flex items-center max-xl:hidden">
           <input className="h-14 px-4 rounded-tl-xl rounded-bl-xl focus:outline-none" placeholder="Find a Movie!" onChange={props.value} />
           <Link to="/search">
             <button className="bg-white p-4 h-14 rounded-tr-xl rounded-br-xl " onClick={props.search}>
@@ -28,13 +36,6 @@ const Nav = (props) => {
             </button>
           </Link>
         </div>
-      </div>
-
-      <div className="hidden max-xl:flex">
-        <button onClick={toggleMenu} className="absolute left-3 top-5 z-20 px-3 py-2 bg-white bg-opacity-80 hover:bg-opacity-100 rounded-lg text-red-800 font-bold text-2xl ">
-          X
-        </button>
-        <HamburgerMenu isOpen={isOpen} toggleMenu={toggleMenu} />
       </div>
     </div>
   );
