f33eae9e959a3d79f3d0f8751d998dea69fb8b86
Amer Kulovic <amer.kulovic@yahoo.com>
2023-11-08 11:11:43 -0600

    edited hamburger menu

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         setMovies(data);
-        if (isOpen) setIsOpen(!isOpen);
+        if (isOpen) toggleMenu();
       });
   };
 
