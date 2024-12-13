93dd85f611c93636583bc52909655aa56a53ca95
Amer Kulovic <amer.kulovic@yahoo.com>
2024-12-13 08:38:12 -0600

    fixed error in route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ mongoose
 
 // Static file serving (for production builds)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
