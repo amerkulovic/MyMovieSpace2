bfa4344c3637b815e6da5270c88ab00e2fa6bfdf
Amer Kulovic <amer.kulovic@yahoo.com>
2025-01-04 13:30:50 -0600

    fixed bug with the footer displaying link to bookmarks page when not logged in

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 // import Logo from "../../images/Logo.png";
 import ContactFooter from "./ContactFooter";
 import FooterSection from "./FooterSection";
+import { useAuth } from "../AuthContext";
 
 const Footer = () => {
   return (
@@ -9,7 +10,7 @@ const Footer = () => {
       <ContactFooter />
       <footer className="bg-gradient-to-r from-black via-red-700 to-black flex flex-wrap justify-between max-sm:justify-center items-center pl-5 py-5">
         <section className="flex flex-row max-md:flex-row max-md:mb-10">
-          <FooterSection header="Discover" item1="Home" item1link="/" item2="Message Board" item2link="/messages" item3="My Bookmarks" item3link="/bookmarks" />
+          <FooterSection header="Discover" item1="Home" item1link="/" item2="Message Board" item2link="/messages" item3="Profile" item3link="/profile" item4="Create Account" item4link="/signup" item5="Login" item5link="/login" />
           <FooterSection header="Contact" item2="(555)-555-5555" />
         </section>
         <div className="flex flex-col items-center mr-10">
--- client/src/components/Footer/FooterSection.js
+++ client/src/components/Footer/FooterSection.js
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../AuthContext";
 
 const FooterSection = (props) => {
+  let { isLoggedIn } = useAuth();
   return (
     <div className="flex flex-col text-white mx-3">
       <h1 className="text-4xl">{props.header}</h1>
@@ -11,15 +13,21 @@ const FooterSection = (props) => {
         <Link to={props.item2link}>
           <li className="py-0.5">{props.item2}</li>
         </Link>
-        <Link to={props.item3link}>
-          <li className="py-0.5">{props.item3}</li>
-        </Link>
-        <Link to={props.item4link}>
-          <li className="py-0.5">{props.item4}</li>
-        </Link>
-        <Link to={props.item5link}>
-          <li className="py-0.5">{props.item5}</li>
-        </Link>
+        {isLoggedIn && (
+          <Link to={props.item3link}>
+            <li className="py-0.5">{props.item3}</li>
+          </Link>
+        )}
+        {!isLoggedIn && (
+          <>
+            <Link to={props.item4link}>
+              <li className="py-0.5">{props.item4}</li>
+            </Link>
+            <Link to={props.item5link}>
+              <li className="py-0.5">{props.item5}</li>
+            </Link>
+          </>
+        )}
       </ul>
     </div>
   );
