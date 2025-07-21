cbaba8b39eb3bbef4e4ae0efd12e9f34d0f487e4
Amer Kulovic <amer.kulovic@yahoo.com>
2025-07-20 22:16:18 -0500

    cleaned up ProfilePage component

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import defaultPhoto from "../images/default.jpg";
 import { useAuth } from "./AuthContext";
 import { useNavigate } from "react-router-dom";
