0e7438d99f374e5ff3f8fd6d5ab6ff676ccdfa16
Amer Kulovic <amer.kulovic@yahoo.com>
2025-07-24 15:14:25 -0500

    tidied up code in SignUp component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,5 +1,4 @@
 import { useRef, useState, useEffect } from "react";
-
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useNavigate } from "react-router-dom";
