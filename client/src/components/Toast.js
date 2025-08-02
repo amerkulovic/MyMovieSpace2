2f68f83ecd72a704ddf519bca5d7f5adf81bfb4d
Amer Kulovic <amer.kulovic@yahoo.com>
2025-08-01 21:35:58 -0500

    edited import in Toast component

--- client/src/components/Toast.js
+++ client/src/components/Toast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Toast = ({ message, onClose }) => {
   const [isVisible, setIsVisible] = useState(true);
